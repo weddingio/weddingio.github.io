31bd65edbf6f5037bba608e823ca53dd24ad2064
weddingio <40872497+weddingio@users.noreply.github.com>
2018-07-06 04:47:03 +0700

    Add files via upload

---
+++ test3/test 3/www.mantenyogya.com/wp-content/plugins/click-to-chat-for-whatsapp/assets/js/appad05.js
@@ -0,0 +1,104 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/**
+ * As now load this only when animations are enable
+ * 
+ * 1. to solve autop issue, when added shortcode in same line.
+ * 2. Animations
+ * 
+ * as right now to reduce request, animations, autop issue added in same file
+ * if js code increases then autop issue and animation make seperate
+ * as animation js load when animation enable.
+ */
+
+// jQuery(document).ready(function ($) {
+jQuery(document).ready(function () {
+
+    // to solve autop issue, when added shortcode in same line.
+    jQuery(".inline_issue").prev("p").css("display", "inline");
+
+    // animations .. 
+    jQuery('.ccw-an').hover(add, remove);
+
+    function add() {
+        // jQuery(this).addClass('animated bounce infinite');
+        jQuery(this).addClass('animated infinite');
+    }
+
+    function remove() {
+        jQuery(this).removeClass('animated infinite');
+    }
+});
+
+/***/ })
+/******/ ]);
